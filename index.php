b696d365ffdbe84acacb619bdbf596f55c7cf028
taufighaqi <taufiqhaqiwijaya@gmail.com>
2021-12-01 15:12:21 +0700

    menambahan paragraf

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
     <title>Coba Git Remote</title>
 </head>
 <body>
-    
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam doloribus explicabo officia maiores ullam vero hic eos obcaecati reiciendis, quos exercitationem minus molestiae odit rerum voluptatum aperiam laboriosam quibusdam amet.</p>
 </body>
 </html>
